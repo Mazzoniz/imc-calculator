d2703f65b9fb2fa677a57426cc3480cb04bee3e9
GUSTAVO MATHEUS MAZONI DOS SANTOS <gustavo.33994@aluno.pr.senac.br>
2025-03-20 17:06:27 -0300

    feat: add field validation in the form before inserting into the table

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,24 +1,27 @@
+// Cálculo do IMC do paciente
 var pacientes = document.querySelectorAll(".paciente");
  
 for (var i = 0; i < pacientes.length; i++) {
     const paciente = pacientes[i];
-    var tdPeso = paciente.querySelector(".info-peso");
-    var tdAltura = paciente.querySelector(".info-altura");
+   
+    var peso = paciente.querySelector(".info-peso").textContent;
+   
+    var altura = paciente.querySelector(".info-altura").textContent;
+ 
+    var alturaEhValida = validaAltura(altura);
+    var pesoEhValido = validaPeso(peso);
  
     var tdImc = paciente.querySelector(".info-imc");
  
-    var alturaEhValida = true;
-    var pesoEhValido = true;
  
-    var peso = tdPeso.textContent;
-    if (peso <= 0 || peso > 600) {
+    if (!pesoEhValido) {
         console.log("Peso inválido!");
         pesoEhValido = false;
         tdImc.textContent = "Peso inválido!"
     }
  
-    var altura = tdAltura.textContent;
-    if (altura <= 0 || altura > 3) {
+ 
+    if (!alturaEhValida) {
         console.log("Altura inválida!");
         alturaEhValida = false;
         tdImc.textContent = "Altura inválida!";
@@ -38,4 +41,21 @@ function calculaImc(peso, altura) {
     var imc = 0;
     imc = peso / Math.pow(altura, 2);
     return imc.toFixed(2);
+}
+ 
+function validaPeso(peso) {
+    if (peso >= 0 && peso < 600) {
+ 
+        return true
+    } else {
+        return false;
+    }
+}
+ 
+function validaAltura(altura) {
+    if (altura >= 0 && altura < 3) {
+        return true
+    } else {
+        return false;
+    }
 }
--- js/form.js
+++ js/form.js
@@ -7,6 +7,13 @@ botaoAdicionar.addEventListener("click", function (event) {
  
     var paciente = obtemPacienteDoFormulario(form);
  
+    var erros = validaPaciente(paciente);
+    if(erros.length > 0){
+        exibeMensagensErro(erros);
+        return;
+    }
+       
+ 
     var pacienteTr = montaTr(paciente);
  
     var tabela = document.querySelector("#tabela-pacientes");
@@ -45,3 +52,42 @@ function montaTr(paciente) {
  
     return pacienteTr;
 }
+ 
+function validaPaciente(paciente) {
+    var erros = [];
+ 
+    if (paciente.nome.length == 0) {
+        erros.push("Nome é obrigatório");
+    }
+    if (paciente.altura.length == 0) {
+        erros.push("Altura é obrigatório");
+    }
+    if (paciente.peso.length == 0) {
+        erros.push("Peso é obrigatório");
+    }
+    if (paciente.gordura.length == 0) {
+        erros.push("Gordura é obrigatório");
+    }
+    if (!validaPeso(paciente.peso)) {
+        erros.push("Peso é inválido");
+    }
+    if (!validaAltura(paciente.altura)) {
+        erros.push("Altura é inválida");
+    }
+ 
+    return erros;
+}
+ 
+ 
+function exibeMensagensErro(erros){
+    var ul = document.querySelector("#msg-erro");
+    ul.innerHTML = "";
+ 
+    erros.forEach(erro => {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    })
+ 
+}
+ 
